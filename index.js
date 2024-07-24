5ca3a1f12cb2e4a68850340cc7fa8f48fb1c1a0f
deby <escuderoflores1993@gmail.com>
2024-07-24 11:50:24 +0200

    se ha creado login.html

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 const express = require("express")
+const bodyParser = require('body-parser');
 const calculadoraNutriCalc = express()
 const port = 3000
 
+calculadoraNutriCalc.use(bodyParser.urlencoded({ extended: true }));
 calculadoraNutriCalc.use(express.static('static'))
+
+// defining a route for the form
+calculadoraNutriCalc.post('/submit', (req, res) => {
+    // accessing form fields from req.body
+    const username = req.body.username;
+    const password = req.body.password;
+    // verification steps 
+    if (!username || !password) {
+        return res.status(400).send('Username and password are required.');
+    }
+    // sending a response
+    res.send(`Received: Username - ${username}, Password - ${password}`);
+});
+
+
 calculadoraNutriCalc.listen(port, () => {
-    console.log(`Calculadora escuchando en http://localhnode ost:${port}`)
+    console.log(`Calculadora escuchando en http://localhost:${port}`)
 })
+
+
+
+
+
--- mvp.js
+++ mvp.js
@@ -11,3 +11,20 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "krono12",
+  database: "nutrikal"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT * FROM usuario", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
