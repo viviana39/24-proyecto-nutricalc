580abdc7098bb14df82011575827182f1eeced9c
JGHJaime <palomojr01@gmail.com>
2024-07-28 20:10:54 +0200

    cambio de puerto

--- src/config/server.js
+++ src/config/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3050);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../app/views'));
 // middlewares
